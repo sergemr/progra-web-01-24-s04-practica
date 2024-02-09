b7a5c1aa9544685f0cdb9d9524d47a318cffcb93
David Camacho <david.camacho.p11@gmail.com>
2024-02-08 18:39:35 -0800

    Formulario David.C

---
+++ miapp/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div Ingresar los datos>
+      <h1>Hola mundo </h1> {nombre}
+      <input type="text"></input>
+      <input type="password"></input> 
+      <br></br>
+      <select>
+        <option>1</option>
+        <option>2</option>
+      </select>
+      <br></br>
+      <button onClick={saludar}>Saludar</button>
+      <button onClick={despedir}>despedir</button>
+
+    
+    </div>
+  );
+}
+
+export default App;
